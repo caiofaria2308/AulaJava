cf66e65c1d1472dee341edd1e6de0304b10a239d
Caio <caiofaria2308@gmail.com>
2019-02-19 21:09:40 -0300

    Adicionando revisão console

---
+++ aplicacaoescola/src/Professor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+public class Professor extends Pessoa {
+    Professor(String nome)
+    {
+    super(nome);
+    }
+   public void DarAula()
+    {
+    System.out.println("Dando aula");
+    }
+    
+}
---
+++ aplicacaoescola/src/aplicacaoescola.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+public class aplicacaoescola 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Pessoa p1=new Professor("Wagnão Maravilhoso");
+       System.out.println(p1.getNome());
+        Professor pf=new Professor("Zé");
+        pf.DarAula();
+    }
+}
+      
+
