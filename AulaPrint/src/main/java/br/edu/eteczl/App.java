734768093d8c859c19e4692af568cfbf5a1356b1
Caio de Faria <caiofaria2308@gmail.com>
2019-02-26 01:30:35 -0300

    Atualização projeto

--- AulaPrint/src/main/java/br/edu/eteczl/App.java
+++ AulaPrint/src/main/java/br/edu/eteczl/App.java
@@ -1,13 +1,35 @@
 package br.edu.eteczl;
-
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+       
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends JFrame
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new App();
+    }
+    public App()
+    {
+    setSize (500,500);
+    setVisible (true);
+    }
+    public void paint(Graphics g)
+    {
+    g.drawRect(50, 50, 100, 25);
+    
+    g.setColor(new Color(00,0,255));
+    g.drawOval(200,50,50,50);
+    
+    g.setColor(Color.GREEN);
+    g.fillRect(50, 200,100, 25);
+    
+    g.setColor(Color.BLACK);
+    g.drawString("My String", 200,200);
+    
     }
 }
